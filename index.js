77a8dd7bd50fdbc2d575e4b312c62c4dc7ac196d
Ngoc Nguyen <nnpk0410@gmail.com>
2023-03-25 12:47:16 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+// function receive 1 argument, an Array of JavaScript Objects
+const superbowlWin = (array) => {
+    // use find() to test each Object to see if the result is "W"
+    const elementObj = array.find(element => element.result === "W");
+    // return the year when the win occurred
+    if (elementObj) {
+        return elementObj["year"];
+    }
+}
+
+
